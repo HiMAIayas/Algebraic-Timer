e42b75c002ec4639bdbf928eff4782fb5facd37b
Phumipas Namjaidee <phumihimal@gmail.com>
2024-04-02 13:17:51 +0700

    change img path to absolute

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,6 @@ const nextConfig = {
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
 
-
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
