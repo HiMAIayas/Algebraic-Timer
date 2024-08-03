d9ab0d31515af6896b04f0fc8f67d321635d7725
Phumipas Namjaidee <phumihimal@gmail.com>
2024-08-03 23:49:18 +0700

    add graph

---
+++ src/components/EquationGraph.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Plot from 'react-plotly.js';
+
+interface Prop{
+    datapoint:{
+        x:Array<Number>,
+        y1:Array<Number>,
+        y2:Array<Number>
+    }
+}
+
+export default function EquationGraph({datapoint}:Prop) {
+  return (
+    <div>
+h
+    </div>
+  )
+}
